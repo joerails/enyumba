47077316a9927d0d29e4c9790b7c0d5dc2b9a93f
joerails <joerails2012@gmail.com>
2012-09-06 08:33:25 +0300

    Initial enyumba

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enyumba::Application.config.secret_token = 'ac2f181199ce5672a2d7c27eeae204fa54d6d400edcece415496016e601484548d1062112451b7e18afce25eada956f6c9347fa16d5d9507a6c05eaf939d141b'
